df5bb50291b3a0b6650ed119ca1693fc91f371bf
Giada Del Testa <g.deltesta@xeel.tech>
2025-02-25 10:55:32 +0100

    init sidenav onclick

--- src/app/sidebar-menu/sidebar-menu.component.ts
+++ src/app/sidebar-menu/sidebar-menu.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-sidebar-menu',
   imports: [
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatSidenavModule
   ],
   templateUrl: './sidebar-menu.component.html',
   styleUrls: ['./sidebar-menu.component.scss']
 })
 export class SidebarMenuComponent {
+  showFiller = false;
 }
